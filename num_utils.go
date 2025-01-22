e82984305604a667174718af7e3a9aa096d9d515
Sergey Ulyashev <sergey.ulyashev@gmail.com>
2025-01-22 20:21:22 +0300

    make fns generic

--- num_utils.go
+++ num_utils.go
@@ -1,22 +1,22 @@
 package num_utils
 
-// type Integer interface {
-// 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-// 		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
-// }
+type Integer interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
 
-// type Float interface {
-// 	~float32 | ~float64
-// }
+type Float interface {
+	~float32 | ~float64
+}
 
-// type Number interface {
-// 	Integer | Float
-// }
+type Number interface {
+	Integer | Float
+}
 
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
 
-func Mul(a, b int) int {
+func Mul[T Number](a, b T) T {
 	return a * b
 }
